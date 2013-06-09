bfeb18249fb6826f0b86c37142ba9f2b7a2ef5c1
huangfulei <huangfl@m15.cn>
2013-06-09 10:16:13 +0800

    Initial Commit

---
+++ CustomeViewWithXib/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomeViewWithXib
+//
+//  Created by 黄富磊 on 13-6-9.
+//  Copyright (c) 2013年 huangfulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CustomeViewWithXib/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CustomeViewWithXib
+//
+//  Created by 黄富磊 on 13-6-9.
+//  Copyright (c) 2013年 huangfulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
