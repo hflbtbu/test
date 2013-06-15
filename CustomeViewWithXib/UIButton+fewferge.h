b629d20aed15b4f38a48ed72a88c0e4b40513ac6
huangfulei <huangfl@m15.cn>
2013-06-15 17:45:19 +0800

    first

---
+++ CustomeViewWithXib/UIButton+fewferge.h
@@ -0,0 +1,13 @@
+//
+//  UIButton+fewferge.h
+//  CustomeViewWithXib
+//
+//  Created by 黄富磊 on 13-6-15.
+//  Copyright (c) 2013年 huangfulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (fewferge)
+
+@end
---
+++ CustomeViewWithXib/UIButton+fewferge.m
@@ -0,0 +1,13 @@
+//
+//  UIButton+fewferge.m
+//  CustomeViewWithXib
+//
+//  Created by 黄富磊 on 13-6-15.
+//  Copyright (c) 2013年 huangfulei. All rights reserved.
+//
+
+#import "UIButton+fewferge.h"
+
+@implementation UIButton (fewferge)
+
+@end
